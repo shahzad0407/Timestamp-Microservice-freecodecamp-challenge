3de790735ddccf962143862e6265d1fa0e5931d1
Shahzad Ahmed Siddiqui <shahzadahmedsiddiqui04@gmail.com>
2024-04-12 16:40:03 +0500

    initial commit

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api/:input", function (req, res) {
+  const input = req.params.input;
+  if (input.indexOf("-") != -1) {
+    const date = new Date(req.params.input);
+    req.unix = Math.floor(date.getTime() / 1000);
+    res.json({ "unix": req.unix, "utc": date.toString() })
+  }else if(/\d/.test(`${input}`)){
+    const date = new Date(input*1000)
+    res.json({ "unix": input, "utc": date.toString() })
+    console.log("working")
+  }else{
+    res.json({ error : "Invalid Date" })
+  }
+  // console.log(input.indexOf("-") + "....")
 });
 
 
-
-// Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
